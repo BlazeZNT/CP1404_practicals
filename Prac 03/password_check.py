32dc881879d0e8727aeb963611be319417695106
Zar Ni Tun <zarnitunkt@gmail.com>
2021-07-28 14:59:07 +0630

    Add password_check program

--- Prac 03/password_check.py
+++ Prac 03/password_check.py
@@ -1,10 +1,20 @@
 def main():
     password = input("Enter a password: ")
+    password = get_password(password)
+    change_asterisk(password)
+
+
+def change_asterisk(password):
+    for char in range(len(password)):
+        print('*', end='')
+
+
+def get_password(password):
     while len(password) < 5 or len(password) > 12:
         print("Invalid password")
         password = input("Enter a password: ")
-    for char in range (len(password)):
-        print('*', end='')
+    return password
+
 
 if __name__ == '__main__':
     main()

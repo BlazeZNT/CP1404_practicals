740cec1c193e143ab5c8e76a653d855708336eb4
Zar Ni Tun <zarnitunkt@gmail.com>
2021-07-28 15:51:40 +0630

    Update file to put the filter into functions

--- Prac 03/broken_score.py
+++ Prac 03/broken_score.py
@@ -3,15 +3,22 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score > 90:
-        print("Excellent")
-    if score > 50:
-        print("Passable")
+def main():
+    score = float(input("Enter score: "))
+    print(change_score_to_result(score))
+
+
+def change_score_to_result(score):
+    if score > 100 or score < 0:
+        return "The score is invalid"
+    if score >= 90:
+        return "Excellent"
+    if score >= 50:
+        return "Passable"
     else:
-        print("Bad")
+        return "Bad"
+
+
+if __name__ == '__main__':
+    main()

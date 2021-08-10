c445eacf140d29c18867ad054286363348c47fa7
Zar Ni Tun <zarnitunkt@gmail.com>
2021-08-10 15:20:06 +0630

    Add for 2 for loops to check maximum number in line

--- Prac 04/quick_picks.py
+++ Prac 04/quick_picks.py
@@ -7,14 +7,15 @@ def main():
     while times_picks <= 0 :
         print("invalid number . Please try again")
         times_picks = int(input("How many quick picks? : "))
-    for i in range (times_picks):
-        quick_pick = []
-        for j in range (maximum_number_in_line):
+    for i in range(times_picks):
+        quick_picks = []
+        for j in range(maximum_number_in_line):
             number = random.randint(minium,maximum)
-            while number not in quick_pick:
-                quick_pick.append(number)
-        quick_pick.sort()
-        print(quick_pick)
+            while number in quick_picks:
+                number = random.randint(minium,maximum)
+            quick_picks.append(number)
+        quick_picks.sort()
+        print(quick_picks)
 
 
 

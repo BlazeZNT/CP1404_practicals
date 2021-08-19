27d656950825a2aad7f20affbc89da780a8983e9
Zar Ni Tun <zarnitunkt@gmail.com>
2021-08-19 19:26:10 +0630

    Refactor the codes

--- Prac 05/word_occurrences.py
+++ Prac 05/word_occurrences.py
@@ -3,13 +3,11 @@ def main():
     words = user_string.split()
     Word_To_Number = {}
 
-
     for word in words:
-        Word_To_Number[word] = Word_To_Number.get(word,0) +1
+        Word_To_Number[word] = Word_To_Number.get(word, 0) + 1
 
     for word, value in Word_To_Number.items():
         print("{:6} : {:5}".format(word, value))
-
 
 
 if __name__ == '__main__':

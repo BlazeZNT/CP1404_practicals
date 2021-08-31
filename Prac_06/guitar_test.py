9880e89309bf92b48e4ff1059d1d96a760320af4
Zar Ni Tun <zarnitunkt@gmail.com>
2021-08-31 14:32:41 +0630

    Update loop and print function

---
+++ Prac_06/guitar_test.py
@@ -0,0 +1,15 @@
+from guitar import Guitar
+
+
+def main():
+
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    print("{} get age() - Expected {}. Got {}".format(gibson.name, gibson.get_age(), gibson.get_age()))
+    print("{} is vintage() - Expected {}. Got {}".format(gibson.name, gibson.is_vintage(), gibson.is_vintage()))
+    tester = Guitar("Tester", 1995, 3000)
+    print("{} get age() - Expected {}. Got {}".format(tester.name, tester.get_age(), tester.get_age()))
+    print("{} is vintage() - Expected {}. Got {}".format(tester.name, tester.is_vintage(), tester.is_vintage()))
+
+
+if __name__ == '__main__':
+    main()
